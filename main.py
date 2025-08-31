2cbd0f9081b41e89a6c367a592d79ff7a98c0945
Snachita_Nandprasad_Gupta <sanchitangaiml123@gst.sies.edu.in>
2025-08-31 13:08:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,7 @@ from app_gradio import create_astrogeo_interface
 app = FastAPI()
 
 gradio_app = create_astrogeo_interface()
-
-# Tell Gradio where it’s mounted
-app = gr.mount_gradio_app(
-    app,
-    gradio_app,
-    path="/astrogeo",
-)
+app = gr.mount_gradio_app(app, gradio_app, path="/astrogeo")
 
 @app.get("/", include_in_schema=False)
 def root():
